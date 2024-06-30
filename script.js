28dedebfb7392c684b54d7c3fa9c0a4215d8c0b4
cheickna <cheicknabarro99@gmail.com>
2024-06-30 17:57:21 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function toggleDetails(id) {
+    const details = document.getElementById(id);
+    if (details.classList.contains('visible')) {
+        details.classList.remove('visible');
+    } else {
+        details.classList.add('visible');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carousels = document.querySelectorAll('.carousel-vertical');
+    carousels.forEach(carousel => {
+        let scrollInterval = setInterval(() => {
+            if (carousel.scrollTop + carousel.clientHeight >= carousel.scrollHeight) {
+                carousel.scrollTop = 0;
+            } else {
+                carousel.scrollTop += 1;
+            }
+        }, 30);
+        carousel.addEventListener('mouseenter', () => clearInterval(scrollInterval));
+        carousel.addEventListener('mouseleave', () => {
+            scrollInterval = setInterval(() => {
+                if (carousel.scrollTop + carousel.clientHeight >= carousel.scrollHeight) {
+                    carousel.scrollTop = 0;
+                } else {
+                    carousel.scrollTop += 1;
+                }
+            }, 30);
+        });
+    });
+});
